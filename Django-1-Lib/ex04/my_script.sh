63a5d3c736b3964d60d9d453a37c2bb15289ff86
Amine-Elbekari <amine.elbekari13@gmail.com>
2026-01-16 22:17:27 +0100

    push ex04

---
+++ Django-1-Lib/ex04/my_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VENV_NAME='django_venv'
+
+if [ ! -d "$VENV_NAME" ]; then
+    python -m venv "$VENV_NAME"
+fi
+if [ -f "$VENV_NAME/Scripts/activate" ]; then
+    ACTIVATE_FILE="$VENV_NAME/Scripts/activate"
+else
+    ACTIVATE_FILE="$VENV_NAME/bin/activate"
+fi
+
+source "$ACTIVATE_FILE"
+pip install -r requirement.txt
+
+exec bash --rcfile <(echo ". ~/.bashrc; . $ACTIVATE_FILE")
