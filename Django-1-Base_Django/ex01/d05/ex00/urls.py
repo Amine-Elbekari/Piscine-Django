a2626752dcb799edecb2311c4d1f59c5ff1875e0
Amine-Elbekari <amine.elbekari13@gmail.com>
2026-01-26 11:24:55 +0100

    remove unnecessary folder and venv

--- Django-1-Base_Django/ex01/d05/ex00/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-# app_name = 'ex00'
-
-urlpatterns = [
-    path('', views.markdown_syntax, name='markdown_syntax')
-]
--- Django-1-Base_Django/ex01/d05/ex00/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-# Create your views here.
-
-def markdown_syntax(request):
-    return render(request, 'ex00/index.html')
